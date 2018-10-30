838016764c8cd4e846fb5913736eb9d8a6220d37
ShantnuS <shantnu@outlook.com>
2018-10-30 13:25:33 +0000

    trasmit test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Main script for reading and trasmitting temperatures 
 import time
+import json
+import urllib.request
 
 def read_temp():
     temp1 = 1
@@ -14,8 +16,42 @@ def transmit(temp):
     print("Transmitted!!!!") 
     return True
 
+def create_tank():
+    
+    return r
+
+def get_tank():
+    item  = json.load(urllib.request.urlopen('http://iglooboiler.appspot.com/tank'))
+    return item
+
+def trasmit_test(tank_id, sensor1, sensor2, sensor3):
+    body = {'tankId': tank_id, 'sensor1': sensor1, 'sensor2': sensor2, 'sensor3': sensor3, 'timeTaken': '4343'}
+    print(body)
+    myurl = "http://iglooboiler.appspot.com/readings"
+    req = urllib.request.Request(myurl)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    jsondata = json.dumps(body)
+    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+    req.add_header('Content-Length', len(jsondataasbytes))
+    print (jsondataasbytes)
+    response = urllib.request.urlopen(req, jsondataasbytes)
+    return response
+
+def get_test():
+    item  = json.load(urllib.request.urlopen('http://iglooboiler.appspot.com/readings'))
+    return item
+
 def main():
+    req = get_tank()
+    print(req)
+    tank_id = req[0]['id']
+    print(trasmit_test(tank_id))
+    print(get_test())
+
+
+'''
     while True:
         temp = read_temp()
         transmit(temp)
         time.sleep(60)
+'''
